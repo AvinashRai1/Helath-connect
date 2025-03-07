a7ba4c69e96d10bc7d9872d8ca1ec624a412cb38
Avinash Rai <100064737+AvinashRai1@users.noreply.github.com>
2025-03-07 11:44:20 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const PORT=process.env.PORT || 4000;   
 app.use(
   cors({
-    origin: "https://helath-connect-4-frontend.onrender.com", // ✅ Your frontend URL
+   
     credentials: true, // ✅ Allow cookies to be sent
     methods: ["GET", "POST", "PUT", "DELETE"], // ✅ Allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // ✅ Allowed headers
